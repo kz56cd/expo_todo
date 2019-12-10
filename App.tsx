ae3e9d32b940730650d46d1130aba24d962d2ff7
kz56cd <kz56cd@gmail.com>
2019-12-10 12:35:27 +0900

    Add base ui parts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  StatusBar, 
+  Platform, 
+  ScrollView 
+} from 'react-native';
+
+// const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
+const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 60 : StatusBar.currentHeight;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.filter}>
+        <Text>Filterがここに配置されます</Text>
+      </View>
+      <ScrollView style={styles.todoList}>
+        <Text>Todoリストがここに配置されます</Text>
+      </ScrollView>
+      <View style={styles.input}>
+        <Text>テキスト入力がここに配置されます</Text>
+      </View>
+      <Text>Todo App!</Text>
     </View>
   );
 }
@@ -13,7 +32,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: STATUSBAR_HEIGHT,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  filter: {
+    height: 30
+  },
+  todoList: {
+    flex: 1
+  },
+  input: {
+    height: 30
+  }
 });
